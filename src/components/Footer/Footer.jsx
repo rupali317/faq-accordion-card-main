c6546d08ba0d51014dc5c805d1ecba68e2b9d6ed
PriyankaRC16 <rupali317@gmail.com>
2024-02-23 23:15:09 +0800

    Added footer section

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,23 @@
+export const Footer = () => {
+  return (
+    <small>
+      Challenge by
+      <a
+        href="https://www.frontendmentor.io/profile/rupali317"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Frontend Mentor
+      </a>
+      . Coded by{" "}
+      <a
+        href="https://www.linkedin.com/in/rupali-rc/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Rupali Roy Choudhury
+      </a>
+      .
+    </small>
+  );
+};
