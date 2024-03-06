a47ce106721ce97713c793acaa807cb2a1802114
PriyankaRC16 <rupali317@gmail.com>
2024-03-06 23:27:51 +0800

    Adding a reminder about the change in code

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("main"));
-const footer = ReactDOM.createRoot(document.getElementById("footer"));
+const footer = ReactDOM.createRoot(document.getElementById("footer")); //or I can do div id="root" and within i have main id="main" annd i put the picture box as children of div id="root" and sibling of main id="main"
 
 root.render(
   <React.StrictMode>
