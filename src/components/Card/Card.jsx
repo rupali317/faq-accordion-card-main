fd43ebf51c1f6e8857af220d90e58d0242669b99
PriyankaRC16 <rupali317@gmail.com>
2024-03-21 16:41:33 +0800

    Setting explicit width and height to improve CLS

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -9,8 +9,16 @@ export const Card = () => {
         <source
           srcSet="/images/illustration-box-desktop.svg"
           media={`(min-width:${DesktopWidth})`}
+          width="0"
+          height="0"
         />
-        <PictureStyles.BoxImage src="" alt="" />
+        <PictureStyles.BoxImage
+          src=""
+          alt=""
+          role="presentation"
+          width="191"
+          height="184"
+        />
         {/* So that it does not appear in mobile version */}
       </picture>
       <main>
@@ -18,11 +26,15 @@ export const Card = () => {
           <source
             srcSet="/images/bg-pattern-desktop.svg"
             media={`(min-width:${DesktopWidth})`}
+            width="240"
+            height="104"
           />
           <PictureStyles.BackgroundImage
             src="/images/bg-pattern-mobile.svg"
             alt=""
             role="presentation"
+            width="966"
+            height="945"
           />
         </picture>
         <picture>
@@ -34,6 +46,8 @@ export const Card = () => {
             src="/images/illustration-woman-online-mobile.svg"
             alt=""
             role="presentation"
+            width="472"
+            height="359"
           />
         </picture>
         <FrequentlyAskedQuestion />
