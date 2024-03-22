f8df927c36d6476ee47a75c395d1cdf1017eac0e
PriyankaRC16 <rupali317@gmail.com>
2024-03-22 22:49:01 +0800

    improving the color contrast

--- src/styles/Global.style.jsx
+++ src/styles/Global.style.jsx
@@ -9,8 +9,8 @@ export const BaseStyles = createGlobalStyle`
         --color-neutral-dark-1: hsl(238, 29%, 17%); /* #1e1f36 */
         --color-neutral-dark-2: hsl(237, 12%, 33%); /* #4b4c5f */
         --color-neutral-dark-3: hsl(240, 7%, 46%); /* #6e6e7d */
-        --color-hover-1: hsl(14, 88%, 65%); /* #f47b56 */
-        --color-hover-2: hsl(56, 95%, 56%); /* #f9eb24 */
+        --color-hover-1: hsl(14, 47%, 46%); /* #f47b56 */
+        --color-hover-2: hsl(56, 100%, 87%); /* #fffbbd*/
         --color-box-shadow: hsla(261, 75%, 27%, 49.72%); /* #35127a */
         --color-background: linear-gradient(hsl(273, 75%, 66%), hsl(240, 74%, 65%));
         
