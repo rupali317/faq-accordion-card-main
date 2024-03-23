7b74090b4ddbe48c7518098cda1035c0cc15344e
PriyankaRC16 <rupali317@gmail.com>
2024-03-23 20:22:21 +0800

    Used Javascript

--- src/components/FrequentlyAskedQuestion2/FrequentlyAskedQuestion2.jsx
+++ src/components/FrequentlyAskedQuestion2/FrequentlyAskedQuestion2.jsx
@@ -1,41 +1,42 @@
 import data from "./data.json";
-import React from 'react';
+import React from "react";
 
 export const FrequentlyAskedQuestion2 = () => {
-  const [closeState, setCloseState] = React.useState(true);
-
-  const toggle = () => {
-    setCloseState(prevState => {
-      console.log("curr state"+!prevState);
-      return !prevState;
-    });
-  }
-
-  const faqList = data.map((item, index) => {
-    return (
-      <section key={index}>
-        <div>
-          <div>
-            <span>{item.question}</span>
-            <img
-              src="/images/icon-arrow-down.svg"
-              alt="Button to toggle the display of the content"
-              width="11"
-              height="8"
-              onClick={toggle}
-            />
-          </div>
-          <p style={closeState?{display:'none'}:{display:'block'}}>{item.answer}</p>
-        </div>
-        <hr />
-      </section>
-    );
-  });
-
   return (
     <>
       <h1>faq</h1>
-      {faqList}
+      {data.map((item, index) => (
+        <Question key={index} item={item} />
+      ))}
     </>
+  );
+};
+
+export const Question = ({ item }) => {
+  const [closeState, setCloseState] = React.useState(true);
+
+  const toggle = () => {
+    setCloseState((prevState) => {
+      return !prevState;
+    });
+  };
+
+  return (
+    <section>
+      <div>
+        <div>
+          <span>{item.question}</span>
+          <img
+            src="/images/icon-arrow-down.svg"
+            alt="Button to toggle the display of the content"
+            width="11"
+            height="8"
+            onClick={toggle}
+          />
+        </div>
+        <p style={{ display: closeState ? "none" : "block" }}>{item.answer}</p>
+      </div>
+      <hr />
+    </section>
   );
 };
